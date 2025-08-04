651f9fc6f380007c18b796ec4c5d35e483484bab
Umesh0910 <unikam18@gmail.com>
2025-08-04 16:14:53 +0000

    Added Print logic

--- activity/activity.go
+++ activity/activity.go
@@ -1,6 +1,7 @@
 package activity
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -8,13 +9,10 @@ import (
 )
 
 type UserActResponse struct {
-	UserName    string `json:"userName"`
-	Activity    string `json:"activity"`
-	Repository  string `json:"repository"`
-	PerformedAt string `json:"performedAt"`
+	Event GitHubEvent
 }
 
-func (u *UserActResponse) GetUserActivity(user string) (*UserActResponse, error){
+func (u *UserActResponse) GetUserActivity(user string) ([]GitHubEvent, error) {
 	log.Println("Getting user activity")
 	resp, err := http.Get("https://api.github.com/users/" + user + "/events")
 	if err != nil {
@@ -22,10 +20,31 @@ func (u *UserActResponse) GetUserActivity(user string) (*UserActResponse, error)
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("Response: %s", body)
-	return u, nil
+
+	// unmarshal actor info
+	var events []GitHubEvent
+	err = json.Unmarshal([]byte(body), &events)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("User is: ", events[0].Actor.Login)
+	return events, nil
 }
-  
+
+func (u *UserActResponse) PrintUserActivity(event []GitHubEvent) {
+	// print the user activity for each event
+	for _, event := range event {
+		switch event.Type {
+		case "PushEvent":
+			fmt.Printf("PushEvent: %s Pushed to %s", event.Actor.Login, event.Repo.Name)
+			// case "CreateEvent":
+			// 	fmt.Println("CreateEvent: ", event.Payload.Ref)
+			// case "PullRequestEvent":
+			// 	fmt.Println("PullRequestEvent: ", event.Payload.Ref)
+		}
+	}
+}
---
+++ activity/structures.go
@@ -0,0 +1,34 @@
+package activity
+
+type GitHubEvent struct {
+	ID        string  `json:"id"`
+	Type      string  `json:"type"`
+	Actor     Actor   `json:"actor"`
+	Repo      Repo    `json:"repo"`
+	Payload   Payload `json:"payload"`
+	Public    bool    `json:"public"`
+	CreatedAt string  `json:"created_at"`
+}
+
+type Actor struct {
+	ID           int    `json:"id"`
+	Login        string `json:"login"`
+	DisplayLogin string `json:"display_login"`
+	GravatarID   string `json:"gravatar_id"`
+	URL          string `json:"url"`
+	AvatarURL    string `json:"avatar_url"`
+}
+
+type Repo struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type Payload struct {
+	Ref          *string `json:"ref"` // nullable
+	RefType      string  `json:"ref_type"`
+	MasterBranch string  `json:"master_branch"`
+	Description  *string `json:"description"` // nullable
+	PusherType   string  `json:"pusher_type"`
+}
